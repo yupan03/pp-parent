21bff9ffcf87cc0a187a745bf7b76aedca3de42f
yupan03 <735997736@qq.com>
2019-08-14 18:38:48 +0800

    提交nacos配置中心

--- pp-web/src/main/java/com/project/WebApplication.java
+++ pp-web/src/main/java/com/project/WebApplication.java
@@ -4,7 +4,6 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
@@ -14,7 +13,6 @@ import feign.Retryer;
 @MapperScan("com.project.dao")
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 @EnableDiscoveryClient
-@EnableFeignClients
 public class WebApplication {
 
     public static void main(String[] args) {
