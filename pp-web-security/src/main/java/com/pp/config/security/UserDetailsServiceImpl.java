ae191cebc3c1fdffa05c15ce21d1d29a9234f201
yupan03 <735997736@qq.com>
2019-09-03 23:33:05 +0800

    删除项目

--- pp-web-security/src/main/java/com/pp/config/security/UserDetailsServiceImpl.java
+++ pp-web-security/src/main/java/com/pp/config/security/UserDetailsServiceImpl.java
@@ -6,7 +6,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class JwtUserDetailsServiceImpl implements UserDetailsService {
+public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
--- pp-web-security/src/main/java/com/pp/config/security/WebSecurityConfigAdapter.java
+++ pp-web-security/src/main/java/com/pp/config/security/WebSecurityConfigAdapter.java
@@ -1,19 +1,17 @@
 package com.pp.config.security;
 
+import javax.annotation.Resource;
+
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import javax.annotation.Resource;
-
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableAutoConfiguration
 public class WebSecurityConfigAdapter extends WebSecurityConfigurerAdapter {
     @Resource
@@ -25,27 +23,32 @@ public class WebSecurityConfigAdapter extends WebSecurityConfigurerAdapter {
 
                 .and().addFilterAfter(tokenFilter, UsernamePasswordAuthenticationFilter.class).authorizeRequests()
 
-                .antMatchers("/swagger-ui.html").permitAll().antMatchers("/swagger-resources/**").permitAll()
-                .antMatchers("/images/**").permitAll().antMatchers("/webjars/**").permitAll().antMatchers("/v2/*")
-                .permitAll().antMatchers("/configuration/*").permitAll()
-                // 对于获取token的rest api要允许匿名访问
-                .antMatchers("/user/eLogin/**").permitAll().antMatchers("/user/login/**").permitAll()
+                .antMatchers("/swagger-ui.html").permitAll()
+                
+                .antMatchers("/swagger-resources/**").permitAll()
 
-                // 过滤通用业务系统的调用
-                .antMatchers("/companySign/cbasCheckCompanySign/**").permitAll()
-                .antMatchers("/companySign/listAllApplyCompanySign/**").permitAll()
-                .antMatchers("/companySign/applyCompanySign/**").permitAll()
+                .antMatchers("/images/**").permitAll()
+
+                .antMatchers("/webjars/**").permitAll()
+
+                .antMatchers("/v2/*").permitAll()
+
+                .antMatchers("/configuration/*").permitAll()
+                
+                // 对于获取token的rest api要允许匿名访问
+                .antMatchers("/user/eLogin/**").permitAll()
+
+                .antMatchers("/user/login/**").permitAll()
 
                 // 下载文件
-                .antMatchers("/files/downLoadFile/**").permitAll().antMatchers("/files/downLoadUpFile/**").permitAll()
+                .antMatchers("/files/downLoadFile/**").permitAll()
 
-                .antMatchers("/companySign/leaderPassCompanySign/**").permitAll()
-                .antMatchers("/companySign/leaderRefuseCompanySign/**").permitAll()
+                .antMatchers("/files/downLoadUpFile/**").permitAll()
 
                 // 所有请求需要身份认证
-                .anyRequest().authenticated().and()
+                .anyRequest().authenticated()
                 // 禁用Session
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         // 禁用缓存
         httpSecurity.headers().cacheControl();
