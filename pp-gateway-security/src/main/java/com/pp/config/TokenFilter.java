829c3b7b53b596d4ece3a947b1634f3f5ee3fe11
yupan03 <735997736@qq.com>
2019-09-18 18:35:19 +0800

    1

--- pp-gateway-security/src/main/java/com/pp/config/TokenFilter.java
+++ pp-gateway-security/src/main/java/com/pp/config/TokenFilter.java
@@ -3,6 +3,7 @@ package com.pp.config;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.server.ServerWebExchange;
 
 import reactor.core.publisher.Mono;
@@ -22,6 +23,14 @@ public class TokenFilter implements GlobalFilter, Ordered {
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        return null;
+        ServerHttpRequest request = exchange.getRequest();
+        String path = request.getURI().getPath();
+
+        // 将不需要token校验的进行过滤
+        
+        
+        // 检验token
+
+        return chain.filter(exchange);
     }
 }
