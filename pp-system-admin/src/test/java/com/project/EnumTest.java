83b573d22685af4c3c87fa28366f3c47e3ba274c
yupan03 <735997736@qq.com>
2020-01-06 19:01:22 +0800

    对象复制工具类

--- pp-system-admin/src/test/java/com/project/EnumTest.java
+++ pp-system-admin/src/test/java/com/project/EnumTest.java
@@ -1,20 +1,19 @@
 package com.project;
 
+import com.project.config.DozerCopyUtil;
 import com.project.constant.status.UserState;
 import com.project.controller.user.param.UserAdd;
 import com.project.entity.dao.dto.UserDTO;
 import com.project.entity.dao.qo.UserQO;
 import com.project.service.CommonService;
-import com.project.utils.BeanCopyUtil;
 import common.utils.IdUtil;
+import org.dozer.Mapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
@@ -22,6 +21,8 @@ import java.util.List;
 public class EnumTest {
     @Autowired
     private CommonService commonService;
+    @Autowired
+    private DozerCopyUtil dozerCopyUtil;
 
     @Test
     public void userState() {
@@ -48,12 +49,10 @@ public class EnumTest {
     public void beanCopy() {
         List<UserDTO> data = commonService.getUserDao().getAllUser(null);
 
-        List<UserAdd> data2 = new ArrayList<>();
+        System.out.println(dozerCopyUtil.copyList(data, UserAdd.class));
 
-        BeanUtils.copyProperties(data, data2);
-        System.out.println(data2);
 
-        System.out.println(BeanCopyUtil.copyList(data, UserAdd.class));
+
 
     }
 }
