e455b68721fcaf2a3bd9dc460ccbda5ed3e81349
yupan <735997736@qq.com>
2020-05-19 08:09:46 +0800

    完善BaseController

--- web/component/pp-common/src/main/java/com/pp/controller/BaseController.java
+++ web/component/pp-common/src/main/java/com/pp/controller/BaseController.java
@@ -6,46 +6,46 @@ import com.pp.common.result.ResultList;
 import com.pp.common.result.ResultObj;
 import com.pp.common.result.ResultPage;
 import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
 
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * controller层公共类
- *
  * @author David
  */
 public class BaseController {
+
     /**
-     * PageHelper数据封装
-     *
-     * @param <T>    实体对象
-     * @param status 返回状态
-     * @param list   列表数据
-     * @return 返回分页数据
+     * 默认返回提示语为success
      */
-    protected <T> ResultPage<T> resultPage(int status, String msg, List<T> list) {
-        PageInfo<T> pageInfo = new PageInfo<>(list);
-        return new ResultPage<>(status, msg, pageInfo.getList(), pageInfo.getSize(), pageInfo.getPageNum(),
-                pageInfo.getTotal());
+    protected String msg = "success";
+
+    protected final <T, E> ResultPage<E> resultPage(List<T> list, Class<E> targetClass) {
+        return this.resultPage(HttpStatus.OK.value(), msg, list, targetClass);
     }
 
-    protected <T, E> ResultPage<E> resultPage(int status, String msg, List<T> list, Class<E> targetClass) {
+    protected final <T, E> ResultPage<E> resultPage(int status, String msg, List<T> list, Class<E> targetClass) {
         PageInfo<T> pageInfo = new PageInfo<>(list);
         List<E> ts = copyList(list, targetClass);
         return new ResultPage<>(status, msg, ts, pageInfo.getPageSize(), pageInfo.getPageNum(),
                 pageInfo.getTotal());
     }
 
-    protected Result result(int status, String msg) {
+    protected final Result result() {
+        return this.result(HttpStatus.OK.value(), msg);
+    }
+
+    protected final Result result(int status, String msg) {
         return new Result(status, msg);
     }
 
-    protected <T> ResultObj<T> resultObj(int status, String msg, T t) {
-        return new ResultObj<>(status, msg, t);
+    protected final <T, E> ResultObj<E> resultObj(T t, Class<E> targetClass) {
+        return this.resultObj(HttpStatus.OK.value(), msg, t, targetClass);
     }
 
-    protected <T, E> ResultObj<E> resultObj(int status, String msg, T t, Class<E> targetClass) {
+    protected final <T, E> ResultObj<E> resultObj(int status, String msg, T t, Class<E> targetClass) {
         E e = null;
         try {
             e = targetClass.newInstance();
@@ -57,20 +57,20 @@ public class BaseController {
         return new ResultObj<>(status, msg, e);
     }
 
-    protected <T> ResultList<T> resultList(int status, String msg, List<T> list) {
-        return new ResultList<>(status, msg, list);
+    protected final <T, E> ResultList<E> resultList(List<T> list, Class<E> targetClass) {
+        return this.resultList(HttpStatus.OK.value(), msg, list, targetClass);
     }
 
-    protected <T, E> ResultList<E> resultList(int status, String msg, List<T> list, Class<E> targetClass) {
+    protected final <T, E> ResultList<E> resultList(int status, String msg, List<T> list, Class<E> targetClass) {
         List<E> ts = copyList(list, targetClass);
         return new ResultList<>(status, msg, ts);
     }
 
-    protected void copy(Object source, Object target) {
+    protected final void copy(Object source, Object target) {
         BeanUtils.copyProperties(source, target);
     }
 
-    protected <T, E> List<T> copyList(List<E> source, Class<T> targetClass) {
+    protected final <T, E> List<T> copyList(List<E> source, Class<T> targetClass) {
         List<T> target = new ArrayList<>();
         if (source != null && !source.isEmpty()) {
             source.forEach(obj -> {
