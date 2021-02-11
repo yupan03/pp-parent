d4eff25f77b1d57e26a47b5547d2952b45b3e0f7
yupan <735997736@qq.com>
2021-02-11 13:35:12 +0800

    增加Mybatis-plus

--- pp-system-admin/src/main/java/com/pp/PpSystemApplication.java
+++ pp-system-admin/src/main/java/com/pp/PpSystemApplication.java
@@ -1,35 +1,17 @@
 package com.pp;
 
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.Banner;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-import java.io.File;
-
 @SpringBootApplication
 @MapperScan("com.pp.dao")
 @EnableDiscoveryClient
 @EnableConfigurationProperties
-public class PpSystemApplication implements CommandLineRunner {
-    @Value("${path}")
-    private String path;
-
-    @Override
-    public void run(String... args) throws Exception {
-        // 初始化文件夹
-        File file = new File(path);
-        if (!file.exists()) {
-            file.mkdirs();
-        }
-        // 对项目做一些初始化动作，比如redis缓存
-
-    }
-
+public class PpSystemApplication {
     public static void main(String[] args) {
         SpringApplication springApplication = new SpringApplication(PpSystemApplication.class);
 
